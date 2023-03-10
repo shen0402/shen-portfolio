f06aa43fa1488da3051397701e58549aeb954634
shen0402 <shenzaijun0402@gmail.com>
2023-03-10 03:23:39 -0800

    fix barba link

--- assets/custom.js
+++ assets/custom.js
@@ -24,6 +24,7 @@ if (document.querySelector('[data-barba="wrapper"]')) {
                     afterEnter(data) {
                         setTimeout(() => {
                             initPage();
+                            document.body.classList.remove('loading');
                         }, 500);
                     }
                 }
